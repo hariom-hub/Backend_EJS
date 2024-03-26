65f4a880e4996d952f921661bc2ec5545fe7ae9d
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-03-26 19:32:02 +0530

    saved

--- index.js
+++ index.js
@@ -2,7 +2,25 @@ const express = require('express');
 
 const app = express();
 
+
+const path = require('path');
+
 const port = 8080;
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "/views"));  //path is a package here
+
+app.get("/", (req, res) => {
+
+    res.render("home.ejs");
+});
+
+
+app.get("/rolldice", (req, res) => {
+
+    res.render("rolldice.ejs");
+})
 
 app.listen(port, () => {
 
